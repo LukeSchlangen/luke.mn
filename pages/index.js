3615ac87e2cc62d7907a754bd1516ca0cde64fcb
Luke Schlangen <lukeschlangen@gmail.com>
2023-02-17 08:05:17 -0600

    feat: simplify merging styles

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,24 @@
 import Head from 'next/head'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import defaultStyles from '../styles/Home.module.css'
 import yellowStyles from '../styles/Yellow.module.css'
 
-
-// Copied from https://stackoverflow.com/a/34749873/3644991
-/**
- * Simple object check.
- * @param item
- * @returns {boolean}
- */
-export function isObject(item) {
-  return (item && typeof item === 'object' && !Array.isArray(item));
-}
-
-/**
- * Deep merge two objects.
- * @param target
- * @param ...sources
- */
-export function mergeDeep(target, ...sources) {
-  if (!sources.length) return target;
-  const source = sources.shift();
-
-  if (isObject(target) && isObject(source)) {
-    for (const key in source) {
-      if (isObject(source[key])) {
-        if (!target[key]) Object.assign(target, { [key]: {} });
-        mergeDeep(target[key], source[key]);
-      } else {
-        Object.assign(target, { [key]: source[key] });
-      }
-    }
-  }
-
-  return mergeDeep(target, ...sources);
+const mergeThemes = (defaultStyles, themeStyles) => {
+  const allStyleKeys = [...new Set([...Object.keys(defaultStyles), ...Object.keys(themeStyles)])]
+  return allStyleKeys.reduce((combinedStyles, key) => ({
+    ...combinedStyles,
+    [key]: `${defaultStyles[key]} ${themeStyles[key] || ''}`
+  }), {});
 }
 
 const theme = {
-  default: structuredClone(defaultStyles),
-  yellow: mergeDeep(structuredClone(defaultStyles), structuredClone(yellowStyles)),
+  default: defaultStyles,
+  yellow: mergeThemes(defaultStyles, yellowStyles),
 }
 
 export default function Home() {
   const [styles, setStyles] = useState(defaultStyles);
-  // const changeStyle = newStyle => {
-  //   const mergedStyles = mergeDeep(defaultStyles, newStyle);
-  //   debugger;
-  //   console.log({mergedStyles})
-  //   setStyles(theme)
-  // }
+
   return (
     <div className={styles.container}>
       <Head>

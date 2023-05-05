d1ef09ae7762b3223b8673f35abd5d3be60a7ddc
Luke Schlangen <lukeschlangen@gmail.com>
2023-05-04 20:52:26 -0500

    fix: prevent using window on server

--- src/app/components/copy-link-icon.tsx
+++ src/app/components/copy-link-icon.tsx
@@ -5,16 +5,17 @@ import { useState } from "react";
 export default function CopyLinkIcon({ id }: { id: string }) {
   const [currentClipboard, setCurrentClipboard] = useState('');
   const copyToClipboard = (id: string) => {
-    if (window?.location?.href && navigator?.clipboard?.writeText)
-    navigator.clipboard.writeText(`${window.location.href}#${id}`)
-    setCurrentClipboard(`${window.location.href}#${id}`);
+    if (window?.location?.href && navigator?.clipboard?.writeText) {
+      navigator.clipboard.writeText(`${window.location.href}#${id}`)
+      setCurrentClipboard(`${window.location.href}#${id}`);
+    }
   }
 
   return (
-    <span id={id} onClick={(event) => {event.preventDefault(); copyToClipboard(id)}}>
+    <span id={id} onClick={(event) => { event.preventDefault(); copyToClipboard(id) }}>
       {' '}
       <span className="opacity-50 text-xs">
-        {`${window.location.href}#${id}` === currentClipboard ? 'Copied' : '🔗'}
+        {`${window?.location?.href}#${id}` === currentClipboard ? 'Copied' : '🔗'}
       </span>
     </span>
   )

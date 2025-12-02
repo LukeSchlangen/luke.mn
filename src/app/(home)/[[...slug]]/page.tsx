0c504fbd3889ad63c09523c56e946faa2aab8e21
Luke Schlangen <lukeschlangen@gmail.com>
2025-12-01 21:26:04 -0600

    fix broken favicons

--- src/app/(home)/[[...slug]]/page.tsx
+++ src/app/(home)/[[...slug]]/page.tsx
@@ -2,6 +2,29 @@ import { Theme } from "../../types";
 import HomePage from "../../components/pages/home-page";
 import pathParser from "../../utils/path-parser";
 import NotFoundPage from "../../components/pages/not-found-page";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params: paramsPromise = Promise.resolve({ slug: [] }),
+}: {
+  params: Promise<{ slug: string[] }>;
+}): Promise<Metadata> {
+  const params = await paramsPromise;
+  const { theme } = pathParser(params.slug);
+
+  let icon = "/favicons/smiling-face.svg";
+  if (theme.vibe === "professional") {
+    icon = "/favicons/briefcase.svg";
+  } else if (theme.vibe === "fun") {
+    icon = "/favicons/party-popper.svg";
+  }
+
+  return {
+    icons: {
+      icon: icon,
+    },
+  };
+}
 
 export default async function Page({
   params: paramsPromise = Promise.resolve({ slug: [] }),
--- src/app/deploy/[[...slug]]/page.tsx
+++ src/app/deploy/[[...slug]]/page.tsx
@@ -1,6 +1,29 @@
 import DeployPage from "../../components/pages/deploy-page";
 import NotFoundPage from "../../components/pages/not-found-page";
 import pathParser from "../../utils/path-parser";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params: paramsPromise = Promise.resolve({ slug: [] }),
+}: {
+  params: Promise<{ slug: string[] }>;
+}): Promise<Metadata> {
+  const params = await paramsPromise;
+  const { theme } = pathParser(params.slug);
+
+  let icon = "/favicons/smiling-face.svg";
+  if (theme.vibe === "professional") {
+    icon = "/favicons/briefcase.svg";
+  } else if (theme.vibe === "fun") {
+    icon = "/favicons/party-popper.svg";
+  }
+
+  return {
+    icons: {
+      icon: icon,
+    },
+  };
+}
 
 export default async function Page({
   params: paramsPromise = Promise.resolve({ slug: [] }),
